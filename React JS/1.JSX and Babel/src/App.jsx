cef46e184c4c242e0cc9c78ebe0137029ef3a8c3
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-08-22 15:33:59 +0530

    add react js jsx and babel

---
+++ React JS/1.JSX and Babel/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>This is a Paragraph</p>
+    </>
+  );
+}
+
+export default App;
