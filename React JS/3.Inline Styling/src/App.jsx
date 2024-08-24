112ed7258467738c2b227f2b43f5f39b19afefdd
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-08-24 11:24:21 +0530

    add react inline styling

---
+++ React JS/3.Inline Styling/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const customStyle = {
+    color: "red",
+    fontSize: "2rem",
+    border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
+function App() {
+    return (
+        <>
+            <h1 style={customStyle}>Hello World!</h1>
+        </>
+    );
+}
+
+export default App;
