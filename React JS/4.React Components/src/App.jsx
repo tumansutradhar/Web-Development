b696556cf4ad96fabe66d1402ae9b2b665e12484
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-08-26 11:48:38 +0530

    add react components

---
+++ React JS/4.React Components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./components/Heading";
+import List from "./components/List";
+
+function App() {
+    return (
+        <>
+            <Heading />
+            <List />
+        </>
+    );
+}
+
+export default App;
