d11222a19c7d7deb6e4e16d73cbc1e8a628bb957
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-09-04 08:28:48 +0530

    add react state

---
+++ React JS/13.Changing Complex State/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [fullName, setFullName] = useState({ fName: "", lName: "" });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName(preValue => {
+      if (name === "fName") {
+        return { fName: value, lName: preValue.lName }
+      } else if (name === "lName") {
+        return { fName: preValue.fName, lName: value }
+      }
+    })
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>Hello {fullName.fName} {fullName.lName}</h1>
+        <form>
+          <input name="fName" onChange={handleChange} placeholder='First Name' value={fullName.fName} />
+          <input name="lName" onChange={handleChange} placeholder='Last Name' value={fullName.lName} />
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
