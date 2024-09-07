b938e7ae494b72b2fa476603c51280e794c6005d
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-09-07 09:48:27 +0530

    add react practice jsx

---
+++ React JS/Practice/1.JSX/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='heading'>My Favourite Foods</h1>
+      <div>
+        <img className='food-img1' src="https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="momo img" />
+        <img className='food-img2' src="https://images.unsplash.com/photo-1612929633738-8fe44f7ec841?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="noodles img" />
+        <img className='food-img3' src="https://images.unsplash.com/photo-1589302168068-964664d93dc0?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="chicken biryani img" />
+      </div>
+    </>
+  )
+}
+
+export default App
