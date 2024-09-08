6a85a2d50f29ce47cb1e94e9846035b87330ebf2
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-09-09 00:29:26 +0530

    add react inline styling

---
+++ React JS/Practice/3.Inline Styling/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: "",
+}
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
+
+function App() {
+  return (
+    <>
+      <h1 className='heading' style={customStyle}>{greeting}</h1>
+    </>
+  )
+}
+export default App
