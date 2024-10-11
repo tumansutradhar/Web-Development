d1e1bfd31747d865f875cc1bb606e98b15a2afb0
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-10-11 23:39:19 +0530

    add app

---
+++ React JS/React JS/08-react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-green-400 font-bold'>React Router Dom</h1>
+    </>
+  )
+}
+
+export default App
