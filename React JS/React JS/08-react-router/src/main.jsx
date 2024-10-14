fb25acafd6c7142ed99e19cf1b477c6129e39e73
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-10-14 22:17:47 +0530

    add updated main

--- React JS/React JS/08-react-router/src/main.jsx
+++ React JS/React JS/08-react-router/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx"
 
-createRoot(document.getElementById('root')).render(
+const router = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+];
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
